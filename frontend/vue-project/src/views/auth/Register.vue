8a0c0e7bd0e9b76d7e238518bae565de31211a4a
Svobodennn <melih.sarac@hotmail.com>
2026-02-11 02:14:03 +0300

    npm install axios, basic Register page

--- frontend/vue-project/src/views/auth/Register.vue
+++ frontend/vue-project/src/views/auth/Register.vue
@@ -1,6 +1,42 @@
 <script setup lang="ts">
+import axios from 'axios'
+import { ref } from 'vue'
 
-</script>
+axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
+axios.defaults.baseURL = 'http://localhost:8000'
+
+const name = ref('')
+const email = ref('')
+const password = ref('')
+const password_confirmation = ref('')
+
+const register = async (e: Event) => {
+    await axios.get('/sanctum/csrf-cookie')
+    
+    try {
+        const response = await axios.post('/api/register', {
+            name: name.value,
+            email: email.value,
+            password: password.value,
+            password_confirmation: password_confirmation.value,
+        })
+        console.log(response.data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+</script>   
+
 <template>
-    <h1>Register</h1>
+    <div>
+        <h1>Register</h1>
+        <form @submit.prevent="register">
+            <input type="text" v-model="name" placeholder="Name" />
+            <input type="email" v-model="email" placeholder="Email" />
+            <input type="password" v-model="password" placeholder="Password" />
+            <input type="password" v-model="password_confirmation" placeholder="Confirm Password" />
+            <button type="submit">Register</button>
+        </form>
+    </div>
 </template>
