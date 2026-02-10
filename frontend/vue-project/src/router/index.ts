97283baeda7949a91476bcc32d4031f4027d3cba
Svobodennn <melih.sarac@hotmail.com>
2026-02-11 01:53:53 +0300

    views created

--- frontend/vue-project/src/router/index.ts
+++ frontend/vue-project/src/router/index.ts
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/auth/Login.vue'
+import Register from '../views/auth/Register.vue'
+import Dashboard from '../views/auth/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+    },
+  ],
 })
 
 export default router
