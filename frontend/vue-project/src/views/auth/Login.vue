3e45a6b16bd3534312385e507784137a24b20de2
Svobodennn <melih.sarac@hotmail.com>
2026-03-04 14:13:52 +0300

    FormKit integrated to login and register forms

--- frontend/vue-project/src/views/auth/Login.vue
+++ frontend/vue-project/src/views/auth/Login.vue
@@ -1,24 +1,14 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
 import axiosInstance from '@/lib/axios'
 import { AxiosError } from 'axios'
+import type { FormKitNode } from '@formkit/core';
 
 interface LoginForm {
     email: string;
     password: string;
 }
 
-const form = reactive<LoginForm>({
-    email: '',
-    password: '',
-})
-
-const errors = reactive({
-    email: [],
-    password: [],
-})
-
-const login = async (payload: LoginForm) => {
+const login = async (payload: LoginForm, node?: FormKitNode) => {
     try {
         await axiosInstance.get('/sanctum/csrf-cookie', {
             baseURL: 'http://localhost:8000',
@@ -28,8 +18,7 @@ const login = async (payload: LoginForm) => {
         console.log(response.data)
     } catch (error) {
         if (error instanceof AxiosError && error.response?.status === 422) {
-            errors.email = error.response.data.errors.email
-            errors.password = error.response.data.errors.password
+            node?.setErrors(error.response.data.errors)
         }
         console.error(error)
     }
@@ -43,40 +32,10 @@ const login = async (payload: LoginForm) => {
         </div>
         <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                <form @submit.prevent="login(form)" class="space-y-6">
-                    <div>
-                        <label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
-                        <div class="mt-1">
-                            <input v-model="form.email" type="email" id="email" required
-                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-brand focus:border-brand sm:text-sm"
-                                placeholder="email@example.com" />
-                            <template v-if="errors.email?.length > 0">
-                                <span v-for="error in errors.email" :key="error" class="text-red-500 text-sm">
-                                    {{ error }}
-                                </span>
-                            </template>
-                        </div>
-                    </div>
-                    <div>
-                        <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                        <div class="mt-1">
-                            <input v-model="form.password" type="password" id="password" required
-                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-brand focus:border-brand sm:text-sm"
-                                placeholder="••••••••" />
-                            <template v-if="errors.password?.length > 0">
-                                <span v-for="error in errors.password" :key="error" class="text-red-500 text-sm">
-                                    {{ error }}
-                                </span>
-                            </template>
-                        </div>
-                    </div>
-                    <div>
-                        <button type="submit"
-                            class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-brand hover:bg-brand-strong focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand">
-                            Login
-                        </button>
-                    </div>
-                </form>
+                <FormKit type="form" submit-label="Login" @submit="login">
+                    <FormKit type="email" label="Email" name="email" />
+                    <FormKit type="password" label="Password" name="password" />
+                </FormKit>
             </div>
         </div>
     </div>
--- frontend/vue-project/src/views/auth/Register.vue
+++ frontend/vue-project/src/views/auth/Register.vue
@@ -2,6 +2,7 @@
 import { reactive } from 'vue'
 import axiosInstance from '@/lib/axios'
 import { AxiosError } from 'axios';
+import type { FormKitNode } from '@formkit/core';
 
 interface RegisterForm {
     name: string;
@@ -17,14 +18,8 @@ const form = reactive<RegisterForm>({
     password_confirmation: '',
 })
 
-const errors = reactive({
-    name: [],
-    email: [],
-    password: [],
-    password_confirmation: [],
-})
 
-const register = async (payload: RegisterForm) => {
+const register = async (payload: RegisterForm, node?: FormKitNode) => {
     try {
         await axiosInstance.get('/sanctum/csrf-cookie', {
             baseURL: 'http://localhost:8000',
@@ -34,10 +29,7 @@ const register = async (payload: RegisterForm) => {
         console.log(response.data)
     } catch (error) {
         if (error instanceof AxiosError && error.response?.status === 422) {
-            errors.name = error.response.data.errors.name
-            errors.email = error.response.data.errors.email
-            errors.password = error.response.data.errors.password
-            errors.password_confirmation = error.response.data.errors.password_confirmation
+            node?.setErrors(error.response.data.errors)
         }
         console.error(error)
     }
@@ -51,69 +43,12 @@ const register = async (payload: RegisterForm) => {
         </div>
         <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                <form @submit.prevent="register(form)" class="space-y-6">
-                    <div>
-                        <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
-                        <div class="mt-1">
-                            <input v-model="form.name" type="text" id="name" required
-                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-brand focus:border-brand sm:text-sm"
-                                placeholder="Full Name" />
-                            <template v-if="errors.name?.length > 0">
-                                <span v-for="error in errors.name" :key="error" class="text-red-500 text-sm">
-                                    {{ error }}
-                                </span>
-                            </template>
-                        </div>
-                    </div>
-                    <div>
-                        <label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
-                        <div class="mt-1">
-                            <input v-model="form.email" type="email" id="email" required
-                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-brand focus:border-brand sm:text-sm"
-                                placeholder="email@example.com" />
-                            <template v-if="errors.email?.length > 0">
-                                <span v-for="error in errors.email" :key="error" class="text-red-500 text-sm">
-                                    {{ error }}
-                                </span>
-                            </template>
-                        </div>
-                    </div>
-                    <div>
-                        <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                        <div class="mt-1">
-                            <input v-model="form.password" type="password" id="password" required
-                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-brand focus:border-brand sm:text-sm"
-                                placeholder="••••••••" />
-                            <template v-if="errors.password?.length > 0">
-                                <span v-for="error in errors.password" :key="error" class="text-red-500 text-sm">
-                                    {{ error }}
-                                </span>
-                            </template>
-                        </div>
-                    </div>
-                    <div>
-                        <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm
-                            Password</label>
-                        <div class="mt-1">
-                            <input v-model="form.password_confirmation" type="password" id="password_confirmation"
-                                required
-                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-brand focus:border-brand sm:text-sm"
-                                placeholder="••••••••" />
-                            <template v-if="errors.password_confirmation?.length > 0">
-                                <span v-for="error in errors.password_confirmation" :key="error"
-                                    class="text-red-500 text-sm">
-                                    {{ error }}
-                                </span>
-                            </template>
-                        </div>
-                    </div>
-                    <div>
-                        <button type="submit"
-                            class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-brand hover:bg-brand-strong focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand">
-                            Register
-                        </button>
-                    </div>
-                </form>
+                <FormKit type="form" submit-label="Register" @submit="register">
+                    <FormKit type="text" label="Name" name="name" />
+                    <FormKit type="email" label="Email" name="email" />
+                    <FormKit type="password" label="Password" name="password" />
+                    <FormKit type="password" label="Confirm Password" name="password_confirmation" />
+                </FormKit>
             </div>
         </div>
     </div>
